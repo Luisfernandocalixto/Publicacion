e1edc33fb8fdf76ba6899b212e6025e02c826453
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-09-18 13:49:24 -0600

    modificando rutas

--- api/index.js
+++ api/index.js
@@ -3,15 +3,16 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const  session  = require('express-session');
 const path = require('path');
-const LocalStrategy = require('passport-local').Strategy;
 const flash = require('connect-flash');
 const passport = require('passport');
 
+
+// server
 const app = express();
 require('../src/database/database.js');
 require('../src/config/passport.js');
 
-require('./');
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '../src/views/'));
 app.engine('.hbs', engine({
@@ -31,20 +32,23 @@ app.use(session({
     saveUninitialized: false,
 }))
 app.use(flash())
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-
 app.use((req, res, next) => {
     res.locals.message = req.flash('error');
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-
+    
     next();
 });
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+})
+
+
 
 
 app.use(require('../src/routes/links.js'));
@@ -53,7 +57,7 @@ app.use(require('../src/routes/notes.js'));
 
 const www = './';
 app.use(express.static(www));
-console.log(`serving ${www}`);
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(app.get('port'), () => {
     console.log(`listening on http://localhost:${app.get('port')}`)
